dfecd67816c45c20de916889e5bcdd951914560c
kimsooyoung <tge1375@naver.com>
2022-02-10 21:58:39 +0900

    Done : msg test

--- unitree_legged_real/src/imu_msg_test.cpp
+++ unitree_legged_real/src/imu_msg_test.cpp
@@ -1,27 +1,27 @@
 
 #include <memory>
-#include "unitree_legged_msgs/msg/cartesian.hpp"
+#include "unitree_legged_msgs/msg/imu.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class CartesianPub : public rclcpp::Node {
 private:
-  rclcpp::Publisher<unitree_legged_msgs::msg::Cartesian>::SharedPtr m_pub;
+  rclcpp::Publisher<unitree_legged_msgs::msg::IMU>::SharedPtr m_pub;
   rclcpp::TimerBase::SharedPtr m_timer;
 
-  unitree_legged_msgs::msg::Cartesian m_cart_msg;
+  unitree_legged_msgs::msg::IMU m_cart_msg;
 
   void timer_callback(){
-      m_cart_msg.x = 1.0;
-      m_cart_msg.y = 2.0;
-      m_cart_msg.z = 3.0;
+      m_cart_msg.quaternion[0] = 1.0;
+      m_cart_msg.quaternion[1] = 2.0;
+      m_cart_msg.quaternion[2] = 3.0;
       m_pub->publish(m_cart_msg);
   }
-
+  
 public:
-  CartesianPub() : Node("unitree_cartesian_pub_node") {
-    RCLCPP_INFO(get_logger(), "Unitree Cartesian Pub Node Created");
+  CartesianPub() : Node("unitree_imu_pub_node") {
+    RCLCPP_INFO(get_logger(), "Unitree IMU Pub Node Created");
 
-    m_pub = create_publisher<unitree_legged_msgs::msg::Cartesian>("/cartesian", 10);
+    m_pub = create_publisher<unitree_legged_msgs::msg::IMU>("/imu", 10);
     m_timer = create_wall_timer(std::chrono::milliseconds(100),
                                 std::bind(&CartesianPub::timer_callback, this));
   }
